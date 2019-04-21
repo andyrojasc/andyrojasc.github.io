caa10671982db8ac78e0ec9d7403340156041edc
andyrojasc <andyrojasucr@gmail.com>
2019-04-20 18:30:30 -0600

    animation fixed

--- assets/js/animation-delay.js
+++ assets/js/animation-delay.js
@@ -2,9 +2,13 @@
     var preload = document.getElementById("preload");
     var load = document.getElementById("load");
     var loading = 0;
-    var id = setInterval(frame, 64);
+    if(loading<=20)
+    {
+        var id = setInterval(frame, 64);
+    }
 
     function frame() {
+        console.log("sds");
         if (loading == 50) {
             clearInterval(id);
             window.open("index-en.html", "_self");
