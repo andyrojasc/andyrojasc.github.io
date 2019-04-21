a39530d174d81f508343a267a8a8f8e1ba67e53a
andyrojasc <andyrojasucr@gmail.com>
2019-04-21 13:05:41 -0600

    minified js

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,4 +29,36 @@ $(document).ready(function(){
     });
 });
 
+jQuery(window).on('load',function(){
+  $("#precarga").delay(1500).fadeOut(1000);
+  $("#ready-screen").delay(2500).fadeIn(1000);
+});
+
+function getMobileOperatingSystem() {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+  if (/android/i.test(userAgent)) {
+      return "Android";
+  }
+
+  if (/iPad|iPhone|Mac|iPod/.test(userAgent) && !window.MSStream) {
+      return "iOS";
+  }
+
+  return "unknown";
+}
+
+function DetectAndServe() {
+  var os = getMobileOperatingSystem();
+  if (os == "Android") {
+      window.open('https://play.google.com/store/apps/details?id=go.muniorotina&hl=en_US', '_blank');
+      //window.location.href = "https://play.google.com/store/apps/details?id=go.muniorotina&hl=en_US"; 
+  } else if (os == "iOS") {
+      window.open('https://itunes.apple.com/cr/app/municipalidad-de-orotina/id1450196328?mt=8', '_blank');
+      //window.location.href = "https://itunes.apple.com/cr/app/municipalidad-de-orotina/id1450196328?mt=8";
+  } else {
+      window.open('https://play.google.com/store/apps/details?id=go.muniorotina&hl=en_US', '_blank');
+      //window.location.href = "https://play.google.com/store/apps/details?id=go.muniorotina&hl=en_US"; 
+  }
+}
 
